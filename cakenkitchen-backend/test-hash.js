d1f5620f7a7cb813f71c9f1ba2c88efacc676086
ghimire-ankit <ankitghimire2004@gmail.com>
2026-07-05 21:55:18 +0545

    Cleanup: Remove diagnostic scripts

--- cakenkitchen-backend/test-hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-bcrypt.hash('admin123', 10).then(hash => {
-    console.log('HASH:', hash);
-    return bcrypt.compare('admin123', hash);
-}).then(res => {
-    console.log('COMPARE:', res);
-});
